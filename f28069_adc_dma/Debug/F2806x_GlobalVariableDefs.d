376949e814304cd9b3a4b036bae7ebf2a53063c2
eduardolmsj <dudump10111@usp.br>
2023-10-31 13:30:00 -0300

    adc+dma code

---
+++ f28069_adc_dma/Debug/F2806x_GlobalVariableDefs.d
@@ -0,0 +1,78 @@
+# FIXED
+
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/source/F2806x_GlobalVariableDefs.c
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Device.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Adc.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Cla.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Comp.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Dma.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECan.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECap.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EQep.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_I2c.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Spi.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Sci.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Usb.h
+F2806x_GlobalVariableDefs.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/source/F2806x_GlobalVariableDefs.c:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Device.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Adc.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Cla.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Comp.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Dma.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECan.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECap.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EQep.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_I2c.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Spi.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Sci.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Usb.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h:
+
---
+++ f28069_adc_dma/Debug/main.d
@@ -0,0 +1,105 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/DSP28x_Project.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Device.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Adc.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Cla.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Comp.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Dma.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECan.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECap.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EQep.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_I2c.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Spi.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Sci.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Usb.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Examples.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h
+main.obj: C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h
+
+../main.c:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/DSP28x_Project.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Cla_typedefs.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Device.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Adc.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_BootVars.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Cla.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Comp.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_CpuTimers.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_DevEmu.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Dma.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECan.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_ECap.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EPwm.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_EQep.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Gpio.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_HRCap.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_I2c.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Mcbsp.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_NmiIntrupt.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieCtrl.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_PieVect.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Spi.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Sci.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_SysCtrl.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_Usb.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/headers/include/F2806x_XIntrupt.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Examples.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_GlobalPrototypes.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_EPwm_defines.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_I2c_defines.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Dma_defines.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_Cla_defines.h:
+
+C:/Users/Eduardo/Desktop/TMS570/c2000/C2000Ware_5_00_00_00/device_support/f2806x/common/include/F2806x_DefaultISR.h:
+
